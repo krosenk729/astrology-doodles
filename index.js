f53f3bfec76ac8be2c5c0af11548d3cfeee5461e
krosenk729 <krosenk729@gmail.com>
2021-01-17 08:26:44 -0500

    progress to fix #1 and #2

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const signs = {
 // Express Middleware (Body Parse, Static Files, View Engines)
 // ===========================================================
 
+app.set("query parser", "extended");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 // app.engine("ejs", require("ejs").__express);
@@ -35,32 +36,45 @@ app.use(cors());
 // Scraping Fns
 // ===========================================================
 
-const astroDotCom = async function (sign) {
-	const url = `https://www.astrology.com/horoscope/daily/${sign}.html`;
+const astroDotCom = async function (sign, isTomorrow = false) {
+	const url = isTomorrow
+		? `https://www.astrology.com/horoscope/daily/tomorrow/${sign}.html`
+		: `https://www.astrology.com/horoscope/daily/${sign}.html`;
 	const res = await fetch(url).then((res) => res.text());
 	const $ = cheerio.load(res);
 	const title = "Astrology Dot Com";
-	const body = $(".horoscope-main.grid > main > p");
+	const body = $(".horoscope-main.grid > main > p:nth-of-type(1)");
 	return { title, body: body.text() };
 };
 
-const truthStar = async function (sign) {
+const truthStar = async function (sign, isTomorrow = false) {
 	const url = `https://www.truthstar.com/daily-horoscope/${sign}-daily-horoscope/`;
 	const res = await fetch(url).then((res) => res.text());
 	const $ = cheerio.load(res);
 	const title = "Truth Star";
-	const body = $(".entry-content h3:nth-of-type(2) ~ p");
-	return { title, body: body.text() };
+	let body = isTomorrow
+		? $(".entry-content h3:nth-of-type(3) ~ p")
+		: $(".entry-content h3:nth-of-type(2) ~ p");
+	body = body[0].children[0].data;
+	return { title, body };
 };
 
-const tarot = async function (sign) {
-	const url = `https://www.tarot.com/daily-horoscope/${sign}`;
+const _getTomorrowFormatted = () => {
+	const dt = new Date();
+	dt.setDate(dt.getDate() + 1);
+	return `${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()}`;
+}
+const tarot = async function (sign, isTomorrow = false) {
+	const url = isTomorrow
+		? `https://www.tarot.com/daily-horoscope/${sign}/${_getTomorrowFormatted()}`
+		: `https://www.tarot.com/daily-horoscope/${sign}`;
 	const res = await fetch(url).then((res) => res.text());
 	const title = "Tarot";
 	let i = res.indexOf("scope_txt");
 	let body = res.substring(i + "scope_txt".length + 3);
 	i = body.indexOf("\"");
 	body = body.substring(0, i);
+	body = decodeURIComponent(body);
 	return { title, body };
 };
 
@@ -73,12 +87,13 @@ app.get("/", async function (req, res) {
 
 app.get("/:sign", async function (req, res) {
 	const sign = req.params.sign.toLowerCase();
+	const isTomorrow = !!req.query.tomorrow;
 	if (!signs[sign]) return res.redirect(`/${signs.cancer}`);
 
 	const horoscopes = await Promise.all([
-		astroDotCom(sign),
-		truthStar(sign),
-		tarot(sign),
+		astroDotCom(sign, isTomorrow),
+		truthStar(sign, isTomorrow),
+		tarot(sign, isTomorrow),
 	]);
 	res.render("template", {
 		horoscopes,
